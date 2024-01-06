bce23f5af7392d0af7b509c3881f3dd3f4f28816
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-06 16:37:13 +0430

    init

--- src/List.js
+++ src/List.js
@@ -6,7 +6,7 @@ const List = ({people}) => {
       {people.map((person) => {
         const {id, name, age, image} = person
         return <article key={id} className='person'>
-          <img src={image} alt="" />
+          <img src={image} alt={name} />
         </article>
       })}
     </>
