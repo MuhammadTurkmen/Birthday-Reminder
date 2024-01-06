20c779fb018fc2f98c1ec6ff6e344e111cbb31a5
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-06 16:29:38 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people, setPeople] = useState(data)
   return <main>
     <section className="container">
       <h3>0 Birthdays today</h3>
