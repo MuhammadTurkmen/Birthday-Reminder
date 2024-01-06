02d899c59acfc994ec05209d5b895507c2eb8905
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-06 16:43:19 +0430

    init

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
 export default [
   {
-    id: 1, 
+    id: 1,
     name: 'Bertie Yates',
     age: 29,
-    image: 
+    image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
