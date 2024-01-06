68677d61e0c419d609557143b47260f1ed900f31
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-06 16:42:38 +0430

    init

--- src/List.js
+++ src/List.js
@@ -6,7 +6,7 @@ const List = ({people}) => {
       {people.map((person) => {
         const {id, name, age, image} = person
         return <article key={id} className='person'>
-          <img src={image} alt={name} />
+          <img src={image} alt={name} /> 
           <div>   
             <h4>{name}</h4>
             <p>{age} years</p>
