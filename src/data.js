aaa1dec31ff2a898f0af118e5d3116e852f2c03d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-06 16:43:50 +0430

    init

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ export default [
   { 
     id: 2,
     name: 'Hester Hogan',
-    age: 32,
+    age: 32, 
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
