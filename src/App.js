f46c1f4efbe845513c0fd33a7e9c317047a999aa
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-01-06 16:24:11 +0430

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <main>
+    <section className="container">
+      
+    </section>
+  </main>;
 }
 
 export default App;
